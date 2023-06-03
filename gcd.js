bad7f999abb251cf2d7264ee91c5cface137a030
deepakjuit <181337@juitsolan.in>
2023-06-04 03:30:15 +0530

    more question sol

---
+++ gcd.js
@@ -0,0 +1,24 @@
+import { readNumberInput } from "./input.js";
+
+const gcd = (a,b) =>{
+    while(a>0 && b>0){
+        if(a>b){
+            a=a-b
+            b=b
+        }else{
+            let temp = b
+            b=a
+            a=temp-a
+        }
+    }
+    return b
+}
+
+const run = async () => {
+    let a = await readNumberInput('First no.: ');
+    let b = await readNumberInput('Second no.: ');
+    let ans = gcd(a,b);
+    console.log(ans);
+}
+
+await run();
